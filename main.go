3f0a104b8f6ec802d4e3a113977f541b2d5a0f3e
bluebeel <saikouah@gmail.com>
2017-12-11 12:46:48 +0100

    Add port env

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/bluebeel/orm/app"
 )
 
 func main() {
+	port := ":" + os.Getenv("PORT")
 	server := &app.App{}
 	server.Initialize()
-	server.Run(":3000")
+	server.Run(port)
 }
